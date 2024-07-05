2968ed004b7c4f56d2795dd4d5a00cbeb470137e
mahesh8177 <maheshhatte656@gmail.com>
2024-07-06 03:24:03 +0530

    add the files

---
+++ mahesh/.ipynb_checkpoints/interface-checkpoint.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, request, jsonify
+from cyber.utils import Anomaly_detection
+import config
+
+app = Flask(__name__)
+
+#################################################################################################################
+#################################### Home Page API ##############################################################
+#################################################################################################################
+
+@app.route('/')
+def performance_model():
+    print('Welcome to Cyber Security model')
+    return render_template('performance.html')
+
+###################################################################################################################
+########################################## Model API ##############################################################
+###################################################################################################################
+
+@app.route('/anomalies_Detection', methods=['POST', 'GET'])
+def get_anomaly_prediction():
+    if request.method == 'POST':
+        print('We are in the POST method')
+        data = request.form
+        protocol_type = data['protocol_type']
+        service = data['service']
+        flag = data['flag']
+        src_bytes = float(data['src_bytes'])
+        dst_bytes = float(data['dst_bytes'])
+        logged_in = float(data['logged_in'])
+        count = float(data['count'])
+        srv_count = float(data['srv_count'])
+        serror_rate = float(data['serror_rate'])
+        srv_serror_rate = float(data['srv_serror_rate'])
+        rerror_rate = float(data['rerror_rate'])
+        srv_rerror_rate = float(data['srv_rerror_rate'])
+        same_srv_rate = float(data['same_srv_rate'])
+        diff_srv_rate = float(data['diff_srv_rate'])
+        dst_host_count = float(data['dst_host_count'])
+        dst_host_srv_count = float(data['dst_host_srv_count'])
+        dst_host_same_srv_rate = float(data['dst_host_same_srv_rate'])
+        dst_host_diff_srv_rate = float(data['dst_host_diff_srv_rate'])
+        dst_host_srv_diff_host_rate = float(data['dst_host_srv_diff_host_rate'])
+        dst_host_serror_rate = float(data['dst_host_serror_rate'])
+        dst_host_srv_serror_rate = float(data['dst_host_srv_serror_rate'])
+        dst_host_srv_rerror_rate = float(data['dst_host_srv_rerror_rate'])
+
+        prediction = Anomaly_detection(protocol_type, service, flag, src_bytes, dst_bytes,
+                                       logged_in, count, srv_count, serror_rate, srv_serror_rate,
+                                       rerror_rate, srv_rerror_rate, same_srv_rate, diff_srv_rate,
+                                       dst_host_count, dst_host_srv_count, dst_host_same_srv_rate,
+                                       dst_host_diff_srv_rate, dst_host_srv_diff_host_rate,
+                                       dst_host_serror_rate, dst_host_srv_serror_rate,
+                                       dst_host_srv_rerror_rate)
+        
+        cyber_prediction = prediction.get_anomaly_prediction()
+
+        return jsonify({'the cyber security prediction is': cyber_prediction})
+    
+    else:
+        print('We are in the GET method')
+        data1 = request.args
+        protocol_type = data1.get('protocol_type')
+        service = data1.get('service')
+        flag = data1.get('flag')
+        src_bytes = float(data1.get('src_bytes'))
+        dst_bytes = float(data1.get('dst_bytes'))
+        logged_in = float(data1.get('logged_in'))
+        count = float(data1.get('count'))
+        srv_count = float(data1.get('srv_count'))
+        serror_rate = float(data1.get('serror_rate'))
+        srv_serror_rate = float(data1.get('srv_serror_rate'))
+        rerror_rate = float(data1.get('rerror_rate'))
+        srv_rerror_rate = float(data1.get('srv_rerror_rate'))
+        same_srv_rate = float(data1.get('same_srv_rate'))
+        diff_srv_rate = float(data1.get('diff_srv_rate'))
+        dst_host_count = float(data1.get('dst_host_count'))
+        dst_host_srv_count = float(data1.get('dst_host_srv_count'))
+        dst_host_same_srv_rate = float(data1.get('dst_host_same_srv_rate'))
+        dst_host_diff_srv_rate = float(data1.get('dst_host_diff_srv_rate'))
+        dst_host_srv_diff_host_rate = float(data1.get('dst_host_srv_diff_host_rate'))
+        dst_host_serror_rate = float(data1.get('dst_host_serror_rate'))
+        dst_host_srv_serror_rate = float(data1.get('dst_host_srv_serror_rate'))
+        dst_host_srv_rerror_rate = float(data1.get('dst_host_srv_rerror_rate'))
+
+        prediction1 = Anomaly_detection(protocol_type, service, flag, src_bytes, dst_bytes,
+                                        logged_in, count, srv_count, serror_rate, srv_serror_rate,
+                                        rerror_rate, srv_rerror_rate, same_srv_rate, diff_srv_rate,
+                                        dst_host_count, dst_host_srv_count, dst_host_same_srv_rate,
+                                        dst_host_diff_srv_rate, dst_host_srv_diff_host_rate,
+                                        dst_host_serror_rate, dst_host_srv_serror_rate,
+                                        dst_host_srv_rerror_rate)
+        cyber_prediction1 = prediction1.get_anomaly_prediction()
+        return jsonify({'the cyber security prediction is': cyber_prediction1})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=config.PORT_NUMBER, debug=False)
---
+++ mahesh/cyber/.ipynb_checkpoints/utils-checkpoint.py
@@ -0,0 +1,105 @@
+import pickle
+import json
+import config
+import numpy as np
+
+class Anomoly_detection():
+    def __init__(self,protocol_type,service,flag,src_bytes,dst_bytes,
+                 logged_in,count,srv_count,serror_rate,srv_serror_rate,
+                 rerror_rate,srv_rerror_rate,same_srv_rate,diff_srv_rate,
+                 dst_host_count,dst_host_srv_count,dst_host_same_srv_rate,
+                 dst_host_diff_srv_rate,dst_host_srv_diff_host_rate,
+                 dst_host_serror_rate,dst_host_srv_serror_rate,
+                 dst_host_srv_rerror_rate):
+        self.protocol_type= protocol_type
+        self.service= service
+        self.flag=flag
+        self.src_bytes=src_bytes
+        self.dst_bytes=dst_bytes
+        self.logged_in=logged_in
+        self.count=count
+        self.srv_count=srv_count
+        self.serror_rate=serror_rate
+        self.srv_serror_rate=srv_serror_rate
+        self.rerror_rate=rerror_rate
+        self.srv_rerror_rate=srv_rerror_rate
+        self.same_srv_rate=same_srv_rate
+        self.diff_srv_rate=diff_srv_rate
+        self.dst_host_count=dst_host_count
+        self.dst_host_srv_count=dst_host_srv_count
+        self.dst_host_same_srv_rate=dst_host_same_srv_rate
+        self.dst_host_diff_srv_rate=dst_host_diff_srv_rate
+        self.dst_host_srv_diff_host_rate=dst_host_srv_diff_host_rate
+        self.dst_host_serror_rate=dst_host_serror_rate
+        self.dst_host_srv_serror_rate=dst_host_srv_serror_rate
+        self.dst_host_srv_rerror_rate=dst_host_srv_rerror_rate
+
+    def load_model(self):
+        with open(config.model_path,'rb') as f:
+            self.model=pickle.load(f)
+
+        with open(config.json_path, 'r') as f:
+            self.project_data=json.load(f)
+    def get_anamoly_prediction(self):
+        self.load_model()
+
+        test_array = np.zeros(22)
+        test_array[0]=self.project_data['protocol_type'][self.protocol_type]
+        test_array[1]=self.project_data['service'][self.service]
+        test_array[2]=self.project_data['flag'][self.flag]
+        test_array[3]=self.src_bytes
+        test_array[4]=self.dst_bytes
+        test_array[5]=self.logged_in
+        test_array[6]=self.count
+        test_array[7]=self.srv_count
+        test_array[8]=self.serror_rate
+        test_array[9]=self.srv_serror_rate
+        test_array[10]=self.rerror_rate
+        test_array[11]=self.srv_rerror_rate
+        test_array[12]=self.same_srv_rate
+        test_array[13]=self.diff_srv_rate
+        test_array[14]=self.dst_host_count
+        test_array[15]=self.dst_host_srv_count
+        test_array[16]=self.dst_host_same_srv_rate
+        test_array[17]=self.dst_host_diff_srv_rate
+        test_array[18]=self.dst_host_srv_diff_host_rate
+        test_array[19]=self.dst_host_serror_rate
+        test_array[20]=self.dst_host_srv_serror_rate
+        test_array[21]=self.dst_host_srv_rerror_rate
+        print('the Test array is',test_array)
+
+        prediction = self.model.predict([test_array])[0]
+        print(f'The Prediction Of Attack is ', prediction)
+        return prediction
+
+if __name__=='__main__':
+ protocol_type= 'icmp'
+ service= 'auth'
+ flag= 'RSTO'
+ src_bytes=145
+ dst_bytes=143
+ logged_in=1
+ count=15
+ srv_count=0
+ serror_rate=1
+ srv_serror_rate=1
+ rerror_rate=0.06
+ srv_rerror_rate=1.00
+ same_srv_rate=0.06
+ diff_srv_rate=0.87
+ dst_host_count=67
+ dst_host_srv_count=90
+ dst_host_same_srv_rate=0.14
+ dst_host_diff_srv_rate=0.170
+ dst_host_srv_diff_host_rate=0.40
+ dst_host_serror_rate=0.20
+ dst_host_srv_serror_rate=0
+ dst_host_srv_rerror_rate=0.10
+ attack = Anomoly_detection(protocol_type,service,flag,src_bytes,dst_bytes,
+                 logged_in,count,srv_count,serror_rate,srv_serror_rate,
+                 rerror_rate,srv_rerror_rate,same_srv_rate,diff_srv_rate,
+                 dst_host_count,dst_host_srv_count,dst_host_same_srv_rate,
+                 dst_host_diff_srv_rate,dst_host_srv_diff_host_rate,
+                 dst_host_serror_rate,dst_host_srv_serror_rate,
+                 dst_host_srv_rerror_rate)
+attack.get_anamoly_prediction()
